05b020f9079f003f53bb1e0f4c8e6eb4dc0ed188
Federico Pasqua (eisterman) <federico.pasqua.96@gmail.com>
2025-10-02 14:30:15 +0200

    Add DaisyUI and WebApp default styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,28 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+
+function Navbar() {
+  return (
+    <div className='navbar bg-base-100 shadow-lg'>
+      <a className='btn btn-ghost text-xl'>React Three.js Test</a>
+    </div>
+  );
+}
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href='https://vite.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
+      <Navbar></Navbar>
       <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+      <div>
+        <button className='btn' onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
     </>
   );
 }
