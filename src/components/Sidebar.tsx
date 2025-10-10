4f1e33483a7d8d3d2099bc8e403da4581e873e05
Federico Pasqua (eisterman) <federico.pasqua.96@gmail.com>
2025-10-10 16:38:19 +0200

    Flex Wrap the buttons in the sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -12,11 +12,10 @@ export function Sidebar({ className }: { className?: string }) {
   const cubes = useProjectStore((state) => state.cubes);
   return (
     <div className={clsx('bg-base-200', className)}>
-      <div className={'flex flex-col items-center gap-2 my-2'}>
+      <div className={'flex flex-row flex-wrap items-center justify-center gap-2 my-2'}>
         <button className='btn btn-info' onClick={() => placeDialog.current.showModal()}>
           Select Place
         </button>
-        <PlaceDialog ref={placeDialog} />
         <button
           className='btn btn-success'
           onClick={() => {
@@ -39,6 +38,7 @@ export function Sidebar({ className }: { className?: string }) {
           Delete Cube
         </button>
       </div>
+      <PlaceDialog ref={placeDialog} />
       <TestArea />
     </div>
   );
