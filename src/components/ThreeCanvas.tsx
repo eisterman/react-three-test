1d3b163c22d8d0c8465d041c6ba07a71e3ba95c2
Federico Pasqua (eisterman) <federico.pasqua.96@gmail.com>
2025-10-21 14:55:04 +0200

    Better MapGrid, and properly manage planeGeometry for MapBase

--- src/components/ThreeCanvas.tsx
+++ src/components/ThreeCanvas.tsx
@@ -7,6 +7,7 @@ import type { Cube, Rectangle, TObject, TObjType } from '@/types.ts';
 import tossoEasyUrl from '@/assets/models/tossoeasy-sp.glb?url';
 import tossoEcoUrl from '@/assets/models/tosso_eco_S-2x1.glb?url';
 import CanvasLoading from '@/components/CanvasLoading.tsx';
+import L from 'leaflet';
 
 // Preload models to prevent strange re-rendering at a model load
 // useGLTF.preload(tossoEasyUrl);
@@ -17,21 +18,33 @@ async function fetchMapObjUrl(l: Rectangle) {
   // TODO Use Protected Token for live
   const accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
   const bbox = `[${l.west},${l.south},${l.east},${l.north}]`; // [lon(min),lat(min),lon(max),lat(max)]
-  const url = `https://api.mapbox.com/styles/v1/mapbox/${mapStyle}/static/${bbox}/800x800?access_token=${accessToken}`;
+  const url = `https://api.mapbox.com/styles/v1/mapbox/${mapStyle}/static/${bbox}/1280x1280?access_token=${accessToken}`;
   const resp = await fetch(url);
   if (!resp.ok) throw new Error('Failed to fetch image');
   const blob = await resp.blob();
   return URL.createObjectURL(blob);
 }
 
+function calculateMaxMeters(mapRectangle: Rectangle) {
+  const bounds = L.latLngBounds(
+    L.latLng(mapRectangle.south, mapRectangle.west),
+    L.latLng(mapRectangle.north, mapRectangle.east),
+  );
+  const widthMeters = bounds.getSouthWest().distanceTo(bounds.getSouthEast());
+  const heightMeters = bounds.getSouthWest().distanceTo(bounds.getNorthWest());
+  return Math.max(widthMeters, heightMeters);
+}
+
 function MapBase() {
   const mapRectangle = useProjectStore((state) => state.mapRectangle);
   const [texture, setTexture] = useState<Texture | null>(null);
+  const maxMeters = mapRectangle && calculateMaxMeters(mapRectangle);
 
   useEffect(() => {
     if (mapRectangle === null) {
       setTexture(null);
     } else {
+      setTexture(null);
       fetchMapObjUrl(mapRectangle)
         .then((url) => {
           const loader = new TextureLoader();
@@ -42,11 +55,19 @@ function MapBase() {
   }, [mapRectangle]);
 
   return texture === null ? null : (
-      <mesh position={[0, 0.01, 0]} rotation={[-Math.PI / 2, 0, 0]}>
-        <planeGeometry args={[40, 40]} />
-        <meshBasicMaterial map={texture} />
-      </mesh>
+      maxMeters && (
+        <mesh position={[0, -0.01, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+          <planeGeometry args={[maxMeters, maxMeters]} />
+          <meshBasicMaterial map={texture} />
+        </mesh>
+      )
     );
+}
+
+function MapGrid() {
+  const mapRectangle = useProjectStore((state) => state.mapRectangle);
+  const fadeDistance = mapRectangle ? calculateMaxMeters(mapRectangle) + 10 : 100;
+  return <Grid infiniteGrid fadeDistance={fadeDistance} fadeFrom={0} />;
 }
 
 function RotatingCube({ cube }: { cube: Cube & { uid: string } }) {
@@ -169,7 +190,7 @@ export default function ThreeCanvas() {
         <MapBase />
         <directionalLight position={[5, 5, 5]} />
         <ambientLight intensity={0.3} />
-        <Grid cellColor={'white'} sectionColor={'red'} infiniteGrid />
+        <MapGrid />
         <CameraControls makeDefault />
       </Suspense>
     </Canvas>
